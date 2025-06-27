a746f6bfa2478190a25f7e83ba6ad642659eaceb
Tucker Hwang <tucker_hwang@berkeley.edu>
2025-06-26 20:57:56 -0700

    Fix alian entrypoint, some refactor

--- alian/entrypoint.sh
+++ alian/entrypoint.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/bash
 
 . /etc/profile.d/z00_lmod.sh
-module use /opt/yasp/software/modules
-module load yasp heppyy alian
-
-# Run the main container command
-echo "Command to be run: $*"
-bash -c "$*"
+/opt/yasp/yaspenv.sh "module load bundle/hepbase heppyy alian && $*"
--- alian/env.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-. /etc/profile.d/z00_lmod.sh
-module use /opt/yasp/software/modules
-module load yasp heppyy alian
